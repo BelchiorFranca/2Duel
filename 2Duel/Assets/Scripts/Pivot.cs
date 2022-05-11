34a7c7e44fd394b3b16cae2102ff3c7d6579bd1e
BelchiorFranca <bmf@cin.ufpe.br>
2022-05-10 22:41:53 -0300

    New Input System para jogar local

--- 2Duel/Assets/Scripts/Pivot.cs
+++ 2Duel/Assets/Scripts/Pivot.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Pivot : MonoBehaviour
 {
     Vector2 direction;
+    Vector2 DeltaPointer;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,11 +16,15 @@ public class Pivot : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        direction = mousePos - (Vector2)transform.position;
+        Vector2 mousePos = Camera.main.ScreenToWorldPoint(direction);
+        //direction = mousePos - (Vector2)transform.position;
+        
         FaceMouse();
     }
 
+    public void Look(InputAction.CallbackContext context){
+      direction = context.ReadValue<Vector2>();
+    }
     void FaceMouse(){
         transform.right = direction;
     }
